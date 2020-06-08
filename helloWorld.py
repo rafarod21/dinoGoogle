78e3100e03f31ac96c824da0c8e95c5a896c881e
Rafael <rafael-rafa2.1@hotmail.com>
2020-06-08 20:30:27 -0300

    Leitura do teclado

---
+++ helloWorld.py
@@ -0,0 +1,23 @@
+'''
+Rede neural
+    Backend
+    Frontend
+
+Dinossauro
+    Posição horizontal fixa
+    Pulo (mudar a posição vertical)
+    
+Cacto
+    Posição vertical fixa
+
+Pássaro
+    Pode nascer em 3 posições diferentes
+    Posição vertical fixa
+
+Chão, Céu
+
+Mudança de cor
+
+'''
+
+print("Hello World!");
